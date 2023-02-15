af3e2f6c71ce90d506519b92d6c4d6f891dd7a4a
jgoyret <j.goyret1475@gmail.com>
2023-02-15 05:07:04 -0300

    enviroment :particles, post, models

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,43 +1,59 @@
 import {
   ScrollControls,
-  useScroll,
   Environment,
-  OrbitControls,
 } from "@react-three/drei";
 import {
   EffectComposer,
   Bloom,
   Noise,
   Vignette,
-  DepthOfField,
-  SSR,
-} from "@react-three/postprocessing";
-import { BlendFunction, GlitchMode } from "postprocessing";
+  ColorDepth,
+  ToneMapping,
+  HueSaturation
+ } from "@react-three/postprocessing";
+import { BlendFunction} from "postprocessing";
 import { Perf } from "r3f-perf";
-import { Suspense, useRef } from "react";
+import { useControls } from 'leva'
+import { Suspense } from "react";
 import Model from "./Model";
+import { SpaceParticles } from "./SpaceParticles";
 
 export default function Experience() {
+
+  const hueEffect = useControls({ hue: { value: 1.38, min: 0, max: 6.28319, step: 0.001 }})
   return (
     <>
       <Perf position="top-left" />
       <EffectComposer>
         <Bloom
           mipmapBlur
-          luminanceThreshold={0.7}
+          luminanceThreshold={0.2}
           luminanceSmoothing={0.1}
           height={300}
         />
-        {/* <Noise opacity={0.02} /> */}
+        <Noise opacity={0.03} />
         <Vignette
           eskil={false}
-          offset={0.1}
-          darkness={0.9}
-          blendFunction={BlendFunction.COLOR}
+          offset={0.}
+          darkness={0.2}
+          blendFunction={BlendFunction.SOFT_LIGHT}
         />
+        <ToneMapping
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          adaptive={true} // toggle adaptive luminance map usage
+          resolution={256} // texture resolution of the luminance map
+          middleGrey={0.8} // middle grey factor
+          maxLuminance={16.0} // maximum luminance
+          averageLuminance={1.0} // average luminance
+          adaptationRate={1.0} // luminance adaptation rate
+        />
+        <ColorDepth bits={[64]}/>
+        <HueSaturation saturation={[0.2]} {...hueEffect}/>
       </EffectComposer>
-      <Environment preset="forest" />
-
+      <Environment preset="forest" background blur={[0.5]} />
+      {/* <Sky turbidity={[10]} distance={[100000]} sunPosition={[1,100,100]} mieDirectionalG={[1]} rayleigh={[0.3]} /> */}
+      {/* <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} /> */}
+      <directionalLight color='green' position={[0,40,0]} intensity={[5]}/>
       <Suspense
         fallback={
           <mesh position-y={0.5} scale={[5, 10, 5]}>
@@ -51,6 +67,9 @@ export default function Experience() {
           <Model />
         </ScrollControls>
       </Suspense>
+      <SpaceParticles count={[5000]}/>
+      {/* <CameraShake maxYaw={0.01} maxPitch={0.01} maxRoll={0.01} yawFrequency={0.5} pitchFrequency={0.5} rollFrequency={0.4} /> */}
+
     </>
   );
 }
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,4 +1,4 @@
-import { useAnimations, useGLTF, useScroll} from '@react-three/drei'
+import { useAnimations, useGLTF, useScroll, Float, Caustics} from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
 import { forwardRef, useEffect } from 'react'
 import * as THREE from 'three'
@@ -10,6 +10,7 @@ export default function Model()
     const model = useGLTF('./leapr2.glb')
     const animations = useAnimations(model.animations, model.scene)
     const actions = animations.actions
+    const childrens = model.children
 
     //START
     useEffect(() =>
@@ -18,28 +19,72 @@ export default function Model()
         {
            actions[action].play()
         }
+        
+        model.scene.traverse(function (child) {
+            if (child.isMesh) {
+                child.castShadow = true
+                child.receiveShadow = true
+
+                if(child.material.name == 'material_cubos')
+                {
+                    child.material.metalness = 1
+                    child.material.roughness = 0.07
+                    child.material.emissiveIntensity = 0
+                    child.material.depthFunc = 3
+                }
+
+                if(child.material.name == 'material_cubos.001')
+                {
+                    child.material.metalness = 0
+                    child.material.roughness = 1
+                    child.material.depthFunc = 1
+                    child.material.emissiveIntensity = 0
+                    console.log(child.material) 
+                }
+
+                if(child.material.name == 'concrete_floor_worn_001')
+                {
+                    child.material.metalness = 0.6
+                    child.material.roughness = 0.2
+                    child.material.depthFunc = 3
+                }
+
+                if(child.material.name == 'Material')
+                {
+                    child.material.metalness = 1
+                    child.material.roughness = 0.15
+                    child.material.color = new THREE.Color('rgb(50,50,50)')
+                    child.material.emissive = new THREE.Color('rgb(0,255,100)') 
+                    child.material.depthFunc = 3 
+                    child.material.emissiveIntensity = 0
+                    child.material.wireframe = true
+                }
+              
+                // console.log(child.material)
+            }
+        }) 
     }, [])
 
     //CUSTOM FUNCTIONS
     let updateFov = (state, offset) =>
     {
         // Remplazar con leapr de valores
-        console.log(offset)
+        // console.log(offset)
         if(offset < 0.03)
         {
-            state.camera.fov = 2
+            state.camera.fov = 1
         }
 
         if(offset > 0.03 && offset < 0.2)
         {
             state.camera.fov = 120
         }
-        else if(offset > 0.2 && offset < 0.45)
+        else if(offset > 0.2 && offset < 0.45) 
             {
-                state.camera.fov = 20
-            } else if (offset > 0.45 && offset < 0.6)
+                state.camera.fov = 10
+            } else if (offset > 0.45 && offset < 1)
                    {
-                        state.camera.fov = 80
+                        state.camera.fov = 60
                    } 
     }
    //UPDATE
@@ -74,7 +119,18 @@ export default function Model()
         }
    }) 
 
-    return (<primitive object={model.scene} scale={ 1 }/>)
+    return (
+        <Float
+        speed={0.2} // Animation speed, defaults to 1
+        rotationIntensity={0.2} // XYZ rotation intensity, defaults to 1
+        floatIntensity={0.2} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+        floatingRange={[-0.1, 0.1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        >
+            <primitive object={model.scene} scale={ 1 }/>
+            
+        </Float>
+            
+    )
 }
 
 useGLTF.preload('./leapr2.glb')
