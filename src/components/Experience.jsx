ed4698015fd44c7d7eb239612eb36d4b20d7a362
mamboespacio <emadobao+1@gmail.com>
2023-02-15 13:49:25 -0300

    apply html

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import {
   ScrollControls,
+  Scroll,
   useScroll,
   Environment,
   OrbitControls,
@@ -16,19 +17,25 @@ import { BlendFunction, GlitchMode } from "postprocessing";
 import { Perf } from "r3f-perf";
 import { Suspense, useRef } from "react";
 import Model from "./Model";
+import About from "./About";
+import Claim from './Claim'
+import Partners from './Partners'
+import Dive from './Dive'
+import Projects from './Projects'
 
 export default function Experience() {
   return (
     <>
-      <Perf position="top-left" />
-      <EffectComposer>
+    
+      {/* <Perf position="top-left" /> */}
+      {/* <EffectComposer>
         <Bloom
           mipmapBlur
           luminanceThreshold={0.7}
           luminanceSmoothing={0.1}
           height={300}
-        />
-        {/* <Noise opacity={0.02} /> */}
+        /> 
+        <Noise opacity={0.02} />
         <Vignette
           eskil={false}
           offset={0.1}
@@ -36,8 +43,8 @@ export default function Experience() {
           blendFunction={BlendFunction.COLOR}
         />
       </EffectComposer>
-      <Environment preset="forest" />
-
+      <Environment preset="forest" /> */}
+      
       <Suspense
         fallback={
           <mesh position-y={0.5} scale={[5, 10, 5]}>
@@ -48,6 +55,13 @@ export default function Experience() {
       >
         {" "}
         <ScrollControls pages={6}>
+          <Scroll html style={{ width: '100%', height: '100%' }}>
+            <Claim/>
+            <Partners/>
+            <About/>
+            <Projects/>
+            <Dive/>
+          </Scroll>
           <Model />
         </ScrollControls>
       </Suspense>
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,28 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './components/Experience'
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
+import Navigation from "./components/Navigation";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+import React from "react";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-<Canvas
-    shadows
-    camera={ {
+  <>
+    <Navigation />
+    
+    <Canvas
+      shadows
+      camera={{
         fov: 45,
         near: 0.01,
         far: 1000,
-        position: [ 4, 3, 20 ],
-    } }
->
-<Experience />
-</Canvas>
-)
+        position: [4, 3, 20],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  </>
+);
