38e9c94c956e8ac728f01f6f0bbed5b2a6757f9d
mamboespacio <emadobao+1@gmail.com>
2023-04-13 18:16:13 -0300

    fix partners size

--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -7,13 +7,13 @@ const Partners = () => {
       <div className="container-fluid h-100">
         <div className="row h-100 align-items-end justify-content-md-center pb-5">  
           <div className="col-6 offset-3 offset-md-0 col-md-3 text-center">
-            <img className="w-100" src="/images/partners/SANDBOX.png"/>
+            <img className="w-75" src="/images/partners/SANDBOX.png"/>
           </div>
           <div className="col-6 offset-3 offset-md-0 col-md-3 text-center">
-            <img className="w-100" src="/images/partners/NVIDIA.png"/>
+            <img className="w-75" src="/images/partners/NVIDIA.png"/>
           </div>
           <div className="col-6 offset-3 offset-md-0 col-md-3 text-center">
-            <img className="w-100" src="/images/partners/DCL.png"/>
+            <img className="w-75" src="/images/partners/DCL.png"/>
           </div>
         </div>
       </div>
